8367f071c77dc9c6bc633a24f3ab968a18a68df1
Jibola Sogbein <ajibola.sogbein@andela.co>
2015-05-20 09:48:56 +0100

    partly added feature to get book image from google books api

--- app/js/controllers/book/add.book.ctrl.js
+++ app/js/controllers/book/add.book.ctrl.js
@@ -1,16 +1,26 @@
 'use strict';
 
-angular.module('Andebooks').controller('addBookCtrl', ['$scope', '$location', '$routeParams', 'bookService', 'userService',function($scope, $location, $routeParams,bookService, userService){
+angular.module('Andebooks').controller('addBookCtrl', ['$scope', '$location', '$routeParams', 'bookService', 'userService', 'googleBookFactory',function($scope, $location, $routeParams,bookService, userService, googleBookFactory){
 
   $scope.addBook = function(bookData,user){
-    // console.log(user);
-    //var id
-    userService.getByUsername(user.username)
-      .success(function(data){
 
-        bookService.addBook(bookData,data._id).success(function(data){
-          $location.path('/books');
+    var title = bookData.title;
+    var author = bookData.author;
+    var titleConcat = title.replace(/ /g, '+');
+    var authorConcat = author.replace(/ /g , '+');
+    var authorTitle = titleConcat + '+inauthor:'+authorConcat;
+
+    googleBookFactory.bookInformation(authorTitle).success(function(data){
+        $scope.bookData.imageUrl = data.items[0].volumeInfo.imageLinks.smallThumbnail;
+
+        //console.log('ishe', $scope.bookData.imageUrl);
+        userService.getByUsername(user.username)
+          .success(function(data){
+
+          bookService.addBook(bookData,data._id).success(function(data){
+            $location.path('/books');
+          });
         });
-      });
+    });
   };
 }]);
