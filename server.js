5eca4c2cb03c820005b6b6154abf2542c49cdd39
Jibola Sogbein <ajibola.sogbein@andela.co>
2015-05-20 10:26:53 +0100

    remove app.get

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static('app'));
+app.use(express.static(__dirname));
 
-app.get('*', function(req, res){
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+// app.get('*', function(req, res){
+//   res.sendFile(path.join(__dirname + '/index.html'));
+// });
 
 app.listen(process.env.PORT || 8000, function(err){
   if(err){
